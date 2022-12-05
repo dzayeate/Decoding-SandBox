b35876a1b47d7ddb757bb0a631796e100d4e8cd9
asrillper <203040118@mail.unpas.ac.id>
2022-12-05 11:40:44 +0700

    membuat landing page v1

--- resources/views/home/layouts/main.blade.php
+++ resources/views/home/layouts/main.blade.php
@@ -6,27 +6,31 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<nav class="navbar navbar-expand-lg style="background-color: #e3f2fd;">
+
+<nav class="navbar navbar-expand-lg" style="background-color: #184C99;">
     <div class="container">
-        <a class="navbar-brand" href="#">Decoding Course</a>
+        <a class="navbar-brand" href="#" style="color: #ffff;">Decoding Course</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" style="color: #ffff;" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" style="color: #ffff;" href="#">Product</a>
                 </li>
-                <li class="d-flex nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #ffff;" href="#">Testi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #ffff;" href="#">Contact</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
-                <button class="btn me-2" type="submit">Login</button>
-                <button class="btn btn-primary btn-lg" style="background-color: #23a6f0; type="submit">JOIN US<i class="fa-solid fa-arrow-right px-3 "></i></button>
+                <button class="btn me-2" type="submit" style="color: #ffff;">Login</button>
+                <button class="btn btn-primary btn-lg" style="background-color: #23a6f0; type="submit">JOIN US<i class="fa-solid fa-arrow-right px-2 "></i></button>
             </form>
         </div>
     </div>
@@ -34,9 +38,28 @@
 
 <body>
     <div class="container">
-        <h1>Bootstrap demo</h1>
-        <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.</p>
+    <div class="row">
+    <div class="col">
+    <br><br>
+        <p>Online Course</p>
+        <br><br>
+        <h1>25K STUDENT</h1>
+        <h1>TRUST ISSUE TO US!</h1>
+        <br><br>
+        <p>We know how large objects will act, but things on a small scale.</p>
+        <br>
+        <form class="d-flex" role="search">
+            <button class="btn btn-primary btn-lg me-3" style="background-color: #23a6f0;" type="submit">Join Us<i></i></button> 
+            <button type="button" class="btn btn-outline-primary p">Learn More</button>     
+        </form>
+        
     </div>
+
+    <div class="col">
+    <img src="img/wpu.jpg" class="rounded float-end mt-5" style="max-width: 400px;" alt="Pak Sandhika"> 
+    </div>
+  </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/2468ec86a4.js" crossorigin="anonymous"></script>
