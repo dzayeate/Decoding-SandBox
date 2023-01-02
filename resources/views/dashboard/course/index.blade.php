44ffafea52cc25a5b4818c98113fbaf696eeb7dc
AyuPutri31 <ayupuutri0gmail.com>
2023-01-02 18:49:09 +0700

    membuat contact us dan about us

--- resources/views/dashboard/course/index.blade.php
+++ resources/views/dashboard/course/index.blade.php
@@ -26,102 +26,120 @@
 
     <!-- Cards Start -->
     <div class="row">
- 
-    <div class="col-md-2 col-sm-12 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/html.jpg" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar HTML</h4></div>
-       Hypertext Markup Language adalah bahasa markah standar untuk dokumen yang dirancang untuk ditampilkan di peramban internet.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
 
- <div class="col-md-2 col-sm-6 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/laravel.jpg" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar Laravel</h4></div>
-       Laravel adalah kerangka kerja aplikasi web berbasis PHP yang sumber terbuka, serta menggunakan konsep Model-View-Controller.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
+        <div class="col-md-2 col-sm-12 mb-3">
+            <div class="card">
+                <img src="img/thumbnail/html.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Belajar HTML</h4>
+                    </div>
+                    Hypertext Markup Language adalah bahasa markah standar untuk dokumen yang dirancang untuk ditampilkan di
+                    peramban internet.
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                </div>
+            </div>
+        </div>
 
- <div class="col-md-2 col-sm-12 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/css.png" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar CSS</h4></div>
-       Cascading Style Sheet merupakan aturan untuk mengatur beberapa komponen dalam sebuah web sehingga akan lebih terstruktur dan seragam.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
+        <div class="col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="img/thumbnail/laravel.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Belajar Laravel</h4>
+                    </div>
+                    Laravel adalah kerangka kerja aplikasi web berbasis PHP yang sumber terbuka, serta menggunakan konsep
+                    Model-View-Controller.
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                </div>
+            </div>
+        </div>
 
- <div class="col-md-2 col-sm-6 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/java.png" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar Javascript</h4></div>
-       JavaScript adalah suatu bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar web.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
+        <div class="col-md-2 col-sm-12 mb-3">
+            <div class="card">
+                <img src="img/thumbnail/css.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Belajar CSS</h4>
+                    </div>
+                    Cascading Style Sheet merupakan aturan untuk mengatur beberapa komponen dalam sebuah web sehingga akan
+                    lebih terstruktur dan seragam.
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                </div>
+            </div>
+        </div>
 
- <div class="col-md-2 col-sm-12 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/boot.png" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar Bootstrap</h4></div>
-       Bootstrap adalah kerangka kerja CSS yang sumber terbuka dan bebas untuk merancang situs web dan aplikasi web yang berisi berbagai templat desain.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
-    <!-- Cards End -->
+        <div class="col-md-2 col-sm-6 mb-3">
+            <div class="card">
+                <img src="img/thumbnail/java.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Belajar Javascript</h4>
+                    </div>
+                    JavaScript adalah suatu bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet
+                    dan dapat bekerja di sebagian besar web.
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                </div>
+            </div>
+        </div>
 
-    <!-- Cards Start -->
-    <div class="row">
- 
- <div class="col-md-2 col-sm-6 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/php.png" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar PHP</h4></div>
-       PHP adalah bahasa scripting tujuan umum diarahkan pengembangan web.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
+        <div class="col-md-2 col-sm-12 mb-3">
+            <div class="card">
+                <img src="img/thumbnail/boot.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Belajar Bootstrap</h4>
+                    </div>
+                    Bootstrap adalah kerangka kerja CSS yang sumber terbuka dan bebas untuk merancang situs web dan aplikasi
+                    web yang berisi berbagai templat desain.
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                </div>
+            </div>
+        </div>
+        <!-- Cards End -->
 
- <div class="col-md-2 col-sm-12 mb-3">
-   <div class="card">
-     <img src="img/thumbnail/pyt.png" class="card-img-top" alt="...">
-     <div class="card-body">
-       <div class="card-title"><h4>Belajar Python</h4></div>
-       Python adalah bahasa pemrograman tujuan umum yang ditafsirkan, tingkat tinggi.
-     </div>
-     <div class="card-footer">
-       <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
-     </div>
-   </div>
- </div>
+        <!-- Cards Start -->
+        <div class="row">
 
-    <!-- Cards End -->
+            <div class="col-md-2 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="img/thumbnail/php.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h4>Belajar PHP</h4>
+                        </div>
+                        PHP adalah bahasa scripting tujuan umum diarahkan pengembangan web.
+                    </div>
+                    <div class="card-footer">
+                        <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                    </div>
+                </div>
+            </div>
 
-@endsection
+            <div class="col-md-2 col-sm-12 mb-3">
+                <div class="card">
+                    <img src="img/thumbnail/pyt.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h4>Belajar Python</h4>
+                        </div>
+                        Python adalah bahasa pemrograman tujuan umum yang ditafsirkan, tingkat tinggi.
+                    </div>
+                    <div class="card-footer">
+                        <a href="https://www.youtube.com/@sandhikagalihWPU" class="card-link">Pelajari</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Cards End -->
+        @endsection
--- resources/views/landingpage/pages/contact.blade.php
+++ resources/views/landingpage/pages/contact.blade.php
@@ -1,5 +1,44 @@
 @extends('landingpage.layouts.main')
 
 @section('landingpage-container')
+    <!--Contact-->
+    <div class="subscribe">
+        <div class="container">
+            <br></br>
+            <div class="subscribe-intro">
+                <h2 class="text-center">Contact Us </h2>
+                <h2 class="text-center">Have any questions? We'd love to hear from you.</h2>
+            </div>
 
-@endsection
+            <!-- Filled Start -->
+            <section class="scroll-section" id="filled">
+                <br></br>
+                <div class="card mb-5 row col-6 ">
+                    <div class="card-body">
+                        <div class="justify-content-center">
+                            <form class="align-item-center">
+                                <div class="mb-3 filled">
+                                    <i data-acorn-icon="at-sign"></i>
+                                    <input class="form-control" placeholder="Email" />
+                                </div>
+                                <div class="mb-3 filled">
+                                    <i data-acorn-icon="user"></i>
+                                    <input class="form-control" type="nama" placeholder="Nama" />
+                                </div>
+                                <div class="mb-3 filled">
+                                    <i data-acorn-icon="mic"></i>
+                                    <input class="form-control" type="telepon" placeholder="Telepon" />
+                                </div>
+                                <div class="mb-3 filled">
+                                    <textarea placeholder="Pesan" class="form-control" rows="2"></textarea>
+                                    <i data-acorn-icon="inbox"></i>
+                                </div>
+                                <button class="btn btn-primary" type="submit">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Filled End -->
+        @endsection
